005c98ce5d34ce133e556d62a7e72caf1d051edb
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-17 18:04:41 +0700

    membuat nim dan nama mahasiswa

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class mahasiswa {
 private:
-int main;
+int nim;
 string nama;
 public:
 mahasiswa();
@@ -13,3 +13,8 @@ mahasiswa(string);
 mahasiswa(int iNim, string iNama);
 void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
